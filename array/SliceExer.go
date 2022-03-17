454006e65652f41cbbc08cd41eab4153a5ca2960
wen-tao-ma <wen-tao.ma@hp.com>
2022-03-17 17:19:02 +0800

    done  Array and Slice

--- array/SliceExer.go
+++ array/SliceExer.go
@@ -11,7 +11,7 @@ func main() {
 	for i := 0; i < 10; i++ {
 		a = append(a, i)
 	}
-	fmt.Println(a)
+	//fmt.Println(a)
 
 	//2.使用内置的sort对数组var intArr=[5]int{6,9,2,5,4}排序
 	var intArr = [5]int{6, 9, 2, 5, 4}
